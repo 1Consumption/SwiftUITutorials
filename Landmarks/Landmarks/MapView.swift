dbc624517561fa2247372a390bb5d9a7a70984b8
1Consumption <hanseop95@gmail.com>
2020-11-22 21:47:08 +0900

    feat: MapView 추가
    - MKMapView는 UIKit의 UIView의 subclass임
    - 따라서 SwiftUI에서 UIKit을 사용하려면 UIViewRepresentable이라는 프로토콜을 채택해야함.
    - makeUIView(context:) 메소드는 UIView의 하위 클래스를 반환하는 메소드임.
    - 앱의 상태가 변경되었을 때, SwiftUI는 update(_:context:) 메소드를 호출하여 UIKit view를 업데이트 함.

---
+++ Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by 신한섭 on 2020/11/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
