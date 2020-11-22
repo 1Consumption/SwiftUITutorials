3878e35813af0e49ed14137566024a32a8015961
1Consumption <hanseop95@gmail.com>
2020-11-22 20:34:55 +0900

    feat: 텍스트 색상 변경
    - modifiers는 view를 래핑하여 view의 속성을 바꾸고, view를 리턴하는 메소드를 말함.
    - view를 반환하는 foregroundColor modifiers를 사용하여 green으로 변경

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.green/*@END_MENU_TOKEN@*/)
     }
 }
 
