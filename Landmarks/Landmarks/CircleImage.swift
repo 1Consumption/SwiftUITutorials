2abc92fe4fbc5fbdc3cdfb718c7f08ee389825cc
1Consumption <hanseop95@gmail.com>
2020-11-22 21:15:35 +0900

    feat: CircleImage 생성
    - style의 모양대로 view를 자르는 clipShape 사용
    - 자신을 포함하는 view의 frame의 중앙에 원을 그리는 Circle style 사용
    - 호출한 view의 위에 view를 그리는 overlay 메소드 사용
    - 테두리만 그리는 stroke 메소드를 통해 view의 테두리 그려 줌

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 신한섭 on 2020/11/22.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
